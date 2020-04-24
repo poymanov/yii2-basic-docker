3210634e889706523d455911618a4c9f86e5c795
poymanov <n.poymanov@gmail.com>
2020-04-24 14:45:39 +0300

    Добавлен README

--- src/config/db/db.php
+++ src/config/db/db.php
@@ -4,8 +4,5 @@ use yii\db\Connection;
 
 return [
     'class' => Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
     'charset' => 'utf8',
 ];
